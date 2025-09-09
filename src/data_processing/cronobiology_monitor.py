ffe077c38fd971818c681a06c675d4f43b40a470
mechmind-dwv <ia.mechmind@gmail.com>
2025-09-09 22:30:00 +0200

    🌌 Añadido análisis cronobiológico y dashboard científico

--- src/data_processing/cronobiology_monitor.py
+++ src/data_processing/cronobiology_monitor.py
@@ -22,3 +22,23 @@ class CronobiologyMonitor:
             'vva_anomalies': self.detect_vva_anomalies(data),
             'stress_index': self.calculate_stress_index(data)
         }
+   class CronobiologyMonitor:
+    def __init__(self):
+        self.rhythms = {
+            'circadian': 24.0,
+            'circaseptan': 168.0,
+            'circatrigintan': 720.0,
+            'circannual': 8760.0
+        }
+    
+    def analyze_biological_rhythms(self, health_data):
+        return {
+            'vascular_variability': self.analyze_vva(health_data),
+            'rhythm_parameters': self.calculate_rhythm_parameters(health_data)
+        }
+    
+    def analyze_vva(self, data):
+        return {
+            'vva_anomalies': self.detect_vva_anomalies(data),
+            'stress_index': self.calculate_stress_index(data)
+        }
